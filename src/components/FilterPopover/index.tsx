fc37acadcd21cb2e3e3718acd942b0842eff1f5b
Ryan Waits <ryan.waits@gmail.com>
2022-05-16 16:38:24 -0500

    add filter popover

---
+++ src/components/FilterPopover/index.tsx
@@ -0,0 +1,126 @@
+import * as React from 'react';
+import {
+  Button,
+  Icon,
+  Link,
+  Image,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  SimpleGrid,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
+
+// Icons
+import { MdFilterList } from 'react-icons/md';
+
+// Components
+import { Card } from '@components/Card';
+import { FilterIcon } from '@components/ProjectsPopover/FilterIcon';
+
+export const FilterPopover = () => (
+  <Popover
+    trigger='hover'
+    openDelay={0}
+    placement='bottom'
+    defaultIsOpen={false}
+    gutter={12}
+  >
+    {({ isOpen, onClose }) => (
+      <>
+        <PopoverTrigger>
+          <Button variant='link' rightIcon={<FilterIcon isOpen={isOpen} />} />
+        </PopoverTrigger>
+        <PopoverContent _focus={{ outline: 'none' }} width='auto' bg='base.900'>
+          <SimpleGrid columns={{ base: 1 }}>
+            <Stack spacing='4' direction='row' p='3'>
+              <Stack spacing='1'>
+                <Card
+                  bg='base.900'
+                  border='none'
+                  minW='150px'
+                  px={{ base: '2', md: '2' }}
+                  py={{ base: '2', md: '2' }}
+                  onClick={onClose}
+                  _hover={{
+                    cursor: 'pointer',
+                    bg: 'base.800',
+                  }}
+                >
+                  <Text px='2' fontSize='sm' fontWeight='regular' color='white'>
+                    All
+                  </Text>
+                </Card>
+                <Card
+                  bg='base.900'
+                  border='none'
+                  minW='150px'
+                  px={{ base: '2', md: '2' }}
+                  py={{ base: '2', md: '2' }}
+                  onClick={onClose}
+                  _hover={{
+                    cursor: 'pointer',
+                    bg: 'base.800',
+                  }}
+                >
+                  <Text px='2' fontSize='sm' fontWeight='regular' color='white'>
+                    Active
+                  </Text>
+                </Card>
+                <Card
+                  bg='base.900'
+                  border='none'
+                  minW='150px'
+                  px={{ base: '2', md: '2' }}
+                  py={{ base: '2', md: '2' }}
+                  onClick={onClose}
+                  _hover={{
+                    cursor: 'pointer',
+                    bg: 'base.800',
+                  }}
+                >
+                  <Text px='2' fontSize='sm' fontWeight='regular' color='white'>
+                    Pending
+                  </Text>
+                </Card>
+                <Card
+                  bg='base.900'
+                  border='none'
+                  minW='150px'
+                  px={{ base: '2', md: '2' }}
+                  py={{ base: '2', md: '2' }}
+                  onClick={onClose}
+                  _hover={{
+                    cursor: 'pointer',
+                    bg: 'base.800',
+                  }}
+                >
+                  <Text px='2' fontSize='sm' fontWeight='regular' color='white'>
+                    Active
+                  </Text>
+                </Card>
+                <Card
+                  bg='base.900'
+                  border='none'
+                  minW='150px'
+                  px={{ base: '2', md: '2' }}
+                  py={{ base: '2', md: '2' }}
+                  onClick={onClose}
+                  _hover={{
+                    cursor: 'pointer',
+                    bg: 'base.800',
+                  }}
+                >
+                  <Text px='2' fontSize='sm' fontWeight='regular' color='white'>
+                    Complete
+                  </Text>
+                </Card>
+              </Stack>
+            </Stack>
+          </SimpleGrid>
+        </PopoverContent>
+      </>
+    )}
+  </Popover>
+);
---
+++ src/components/ProjectsPopover/FilterIcon.tsx
@@ -0,0 +1,11 @@
+import { Icon, IconProps } from '@chakra-ui/react';
+import * as React from 'react';
+import { MdFilterList } from 'react-icons/md';
+
+interface FilterIconProps extends IconProps {
+  isOpen: boolean;
+}
+
+export const FilterIcon = (props: FilterIconProps) => {
+  return <Icon aria-hidden as={MdFilterList} />;
+};
