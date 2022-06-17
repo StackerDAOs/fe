48ec7b31a3998fb60962862cd1997470966ae984
Ryan Waits <ryan.waits@gmail.com>
2022-06-16 23:28:34 -0500

    render assets table

--- src/pages/d/[dao].tsx
+++ src/pages/d/[dao].tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import {
   Avatar,
   Box,
+  ButtonGroup,
   Container,
   SimpleGrid,
   Stack,
@@ -23,8 +24,8 @@ import { fetchReadOnlyFunction } from 'micro-stacks/api';
 
 // Components
 import { AppLayout } from '@components/Layout/AppLayout';
-import { Card } from '@components/Card';
 import { Header } from '@components/Header';
+import { AssetTable } from '@components/AssetTable';
 
 // Hooks
 import { useBalance } from '@common/hooks';
@@ -151,13 +152,13 @@ const DAODashboard = () => {
                 >
                   <Box>
                     <Text fontSize='2xl' fontWeight='medium'>
-                      Vault
+                      Assets
                     </Text>
                     <Text color='gray.900' fontSize='sm'>
                       List of shared assets owned by the DAO.
                     </Text>
                   </Box>
-                  <Link href={`/d/${dao}/vault`}>
+                  {/* <Link href={`/d/${dao}/vault`}>
                     <Text
                       as='a'
                       cursor='pointer'
@@ -168,7 +169,7 @@ const DAODashboard = () => {
                     >
                       View more
                     </Text>
-                  </Link>
+                  </Link> */}
                 </Stack>
               </Stack>
               <motion.div
@@ -178,177 +179,58 @@ const DAODashboard = () => {
                 exit={FADE_IN_VARIANTS.exit}
                 transition={{ duration: 0.25, type: 'linear' }}
               >
-                <SimpleGrid
-                  columns={{ base: 1, md: 2, lg: 2 }}
-                  spacing='6'
-                  mb='10'
-                  color='white'
-                >
-                  <Box>
-                    <Card
-                      bg='base.800'
-                      position='relative'
-                      pt={{ base: '3', md: '3' }}
-                      border='1px solid'
-                      borderColor='base.500'
-                      _hover={{
-                        cursor: 'pointer',
-                      }}
-                    >
-                      <Box
-                        as='section'
-                        pb={{ base: '3', md: '3' }}
-                        px={{ base: '3', md: '3' }}
-                      >
-                        <Box pb='3'>
-                          <Text fontSize='md' fontWeight='medium'>
-                            Tokens
-                          </Text>
-                        </Box>
-                        <VStack
-                          align='left'
-                          maxW='md'
-                          spacing='3'
-                          direction={{
-                            base: 'column',
-                            md: 'row',
-                          }}
-                          justify='space-between'
-                          color='white'
+                <Tabs color='white' variant='unstyled'>
+                  <TabList>
+                    <ButtonGroup bg='base.800' borderRadius='lg' p='1'>
+                      {['Coins', 'Collectibles'].map((item) => (
+                        <Tab
+                          key={item}
+                          fontSize='sm'
+                          borderRadius='lg'
+                          color='gray.900'
+                          px='5'
+                          isFullWidth
+                          w='50%'
+                          _selected={{ bg: 'base.500', color: 'light.900' }}
                         >
-                          {assets.map(
-                            ({ name, symbol, image, balance }, index) => (
-                              <Stack
-                                key={index}
-                                color='white'
-                                bg='base.800'
-                                p='3'
-                                borderRadius='lg'
-                              >
-                                <Stack
-                                  direction='row'
-                                  spacing='3'
-                                  display='flex'
-                                  justifyContent='space-between'
-                                >
-                                  <HStack spacing='3'>
-                                    <Avatar
-                                      borderRadius='lg'
-                                      src={image}
-                                      boxSize='6'
-                                    />
-                                    <HStack spacing='1' align='baseline'>
-                                      <Text
-                                        fontSize='sm'
-                                        fontWeight='medium'
-                                        color='light.900'
-                                      >
-                                        {name}
-                                      </Text>
-                                      <Text
-                                        fontSize='xs'
-                                        fontWeight='regular'
-                                        color='gray.900'
-                                      >
-                                        {symbol}
-                                      </Text>
-                                    </HStack>
-                                  </HStack>
-                                  <HStack spacing='1'>
-                                    <Text
-                                      fontSize='sm'
-                                      fontWeight='regular'
-                                      color='light.900'
-                                    >
-                                      {balance}
-                                    </Text>
-                                  </HStack>
-                                </Stack>
-                              </Stack>
-                            ),
-                          )}
-                        </VStack>
-                      </Box>
-                    </Card>
-                  </Box>
-                  <Box>
-                    <Card
-                      bg='base.800'
-                      position='relative'
-                      pt={{ base: '3', md: '3' }}
-                      border='1px solid'
-                      borderColor='base.500'
-                      _hover={{
-                        cursor: 'pointer',
-                      }}
-                    >
-                      <Box
-                        as='section'
-                        pb={{ base: '3', md: '3' }}
-                        px={{ base: '3', md: '3' }}
+                          {item}
+                        </Tab>
+                      ))}
+                    </ButtonGroup>
+                  </TabList>
+                  <TabPanels>
+                    <TabPanel px='0'>
+                      <motion.div
+                        variants={FADE_IN_VARIANTS}
+                        initial={FADE_IN_VARIANTS.hidden}
+                        animate={FADE_IN_VARIANTS.enter}
+                        exit={FADE_IN_VARIANTS.exit}
+                        transition={{ duration: 0.25, type: 'linear' }}
                       >
-                        <Box pb='3'>
-                          <Text fontSize='md' fontWeight='medium'>
-                            NFTs
-                          </Text>
-                        </Box>
-                        <VStack
-                          align='left'
-                          maxW='md'
-                          spacing='3'
-                          direction={{
-                            base: 'column',
-                            md: 'row',
-                          }}
-                          justify='space-between'
-                          color='white'
-                        >
-                          {nfts.map(({ name, image, amount }) => (
-                            <Stack
-                              color='white'
-                              bg='base.800'
-                              p='3'
-                              borderRadius='lg'
-                            >
-                              <Stack
-                                direction='row'
-                                spacing='3'
-                                display='flex'
-                                justifyContent='space-between'
-                              >
-                                <HStack spacing='3'>
-                                  <Avatar
-                                    borderRadius='lg'
-                                    src={image}
-                                    boxSize='6'
-                                  />
-                                  <HStack spacing='1' align='baseline'>
-                                    <Text
-                                      fontSize='sm'
-                                      fontWeight='medium'
-                                      color='light.900'
-                                    >
-                                      {name}
-                                    </Text>
-                                  </HStack>
-                                </HStack>
-                                <HStack spacing='1'>
-                                  <Text
-                                    fontSize='sm'
-                                    fontWeight='regular'
-                                    color='light.900'
-                                  >
-                                    {amount}
-                                  </Text>
-                                </HStack>
-                              </Stack>
-                            </Stack>
-                          ))}
-                        </VStack>
-                      </Box>
-                    </Card>
-                  </Box>
-                </SimpleGrid>
+                        <AssetTable
+                          color='light.900'
+                          size='lg'
+                          type='fungible'
+                        />
+                      </motion.div>
+                    </TabPanel>
+                    <TabPanel px='0'>
+                      <motion.div
+                        variants={FADE_IN_VARIANTS}
+                        initial={FADE_IN_VARIANTS.hidden}
+                        animate={FADE_IN_VARIANTS.enter}
+                        exit={FADE_IN_VARIANTS.exit}
+                        transition={{ duration: 0.25, type: 'linear' }}
+                      >
+                        <AssetTable
+                          color='light.900'
+                          size='lg'
+                          type='nonFungible'
+                        />
+                      </motion.div>
+                    </TabPanel>
+                  </TabPanels>
+                </Tabs>
               </motion.div>
             </Container>
           </Stack>
