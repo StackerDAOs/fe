556304df39aaf517556dfd37b0efb11253a15a86
Ryan Waits <ryan.waits@gmail.com>
2022-06-19 02:05:41 -0500

    testnet check

--- src/common/with-micro-stacks.tsx
+++ src/common/with-micro-stacks.tsx
@@ -1,22 +1,20 @@
 import { wrapWithMicroStacks } from '@micro-stacks/nextjs';
-import {
-  StacksMainnet,
-  StacksTestnet,
-  StacksMocknet,
-} from 'micro-stacks/network';
+import { StacksTestnet } from 'micro-stacks/network';
 
-let network: any;
-switch (process.env.NODE_ENV) {
-  case 'development':
-    network = new StacksMocknet();
-    break;
-  case 'production':
-    network = new StacksMainnet({ url: 'https://mainnet.syvita.org' });
-    break;
-  default:
-    network = new StacksTestnet({ url: 'https://testnet.syvita.org' });
-    break;
-}
+// let network: any;
+// switch (process.env.NODE_ENV) {
+//   case 'development':
+//     network = new StacksMocknet();
+//     break;
+//   case 'production':
+//     network = new StacksMainnet();
+//     break;
+//   default:
+//     network = new StacksTestnet();
+//     break;
+// }
+
+const network = new StacksTestnet();
 
 const authOptions = {
   appDetails: {
