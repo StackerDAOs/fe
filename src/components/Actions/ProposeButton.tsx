90bbb4bc974426b298b0a779ebb7fd8e59fa955d
Ryan Waits <ryan.waits@gmail.com>
2022-06-24 18:39:42 -0500

    add use token hook

---
+++ src/components/Actions/ProposeButton.tsx
@@ -0,0 +1,105 @@
+import { useEffect, useState } from 'react';
+import { supabase } from '@utils/supabase';
+
+// Components
+import { ContractCallButton } from '@widgets/ContractCallButton';
+
+// Web3
+import { useUser } from '@micro-stacks/react';
+import { contractPrincipalCV, uintCV } from 'micro-stacks/clarity';
+
+// Hooks
+import {
+  useBlocks,
+  useGovernanceTokenExtension,
+  useSubmissionExtension,
+} from '@common/hooks';
+import { useUpdate } from 'react-supabase';
+
+export const ProposeButton = ({ organization, transactionId }: any) => {
+  const [state, setState] = useState<any>({ proposalContractAddress: null });
+  const { currentBlockHeight } = useBlocks();
+  const {
+    contractName: governanceContractName,
+    contractAddress: governanceContractAddress,
+  } = useGovernanceTokenExtension({ organization: organization });
+  const { contractName, contractAddress } = useSubmissionExtension({
+    organization: organization,
+  });
+
+  const [_, execute] = useUpdate('Proposals');
+
+  useEffect(() => {
+    const fetchProposal = async (transactionId: string) => {
+      try {
+        const formattedTransactionId = transactionId.substring(2);
+        const { data: Proposals, error } = await supabase
+          .from('Proposals')
+          .select('contractAddress')
+          .eq('transactionId', formattedTransactionId);
+        if (error) throw error;
+        if (Proposals.length > 0) {
+          const proposal = Proposals[0];
+          setState({
+            ...state,
+            proposalContractAddress: proposal.contractAddress,
+          });
+        }
+      } catch (error) {
+        console.log({ error });
+      } finally {
+        console.log('done');
+      }
+    };
+    fetchProposal(transactionId);
+  }, [transactionId]);
+
+  const { proposalContractAddress } = state;
+  const onFinishUpdate = async (proposalContractAddress: string) => {
+    try {
+      const { error } = await execute({ submitted: true }, (q) =>
+        q.eq('contractAddress', proposalContractAddress),
+      );
+      if (error) throw error;
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+
+  const startHeight = currentBlockHeight + 15; // TODO: 15 needs to be dynamic startBlockHeight min
+
+  const isLoading =
+    proposalContractAddress &&
+    governanceContractAddress &&
+    governanceContractName;
+  const functionName = 'propose';
+  const functionArgs = isLoading && [
+    contractPrincipalCV(
+      proposalContractAddress.split('.')[0],
+      proposalContractAddress.split('.')[1],
+    ),
+    uintCV(startHeight),
+    contractPrincipalCV(governanceContractAddress, governanceContractName),
+  ];
+  const postConditions: any = [];
+
+  const contractData = {
+    contractAddress,
+    contractName,
+    functionName,
+    functionArgs,
+    postConditions,
+  };
+
+  return (
+    <ContractCallButton
+      title='Propose'
+      color='white'
+      bg='secondary.900'
+      size='sm'
+      isFullWidth
+      onContractCall={() => onFinishUpdate(proposalContractAddress)}
+      {...contractData}
+    />
+  );
+};
---
+++ src/components/Wrapper/index.tsx
@@ -0,0 +1,24 @@
+import { Container } from '@chakra-ui/react';
+
+// Animations
+import { motion } from 'framer-motion';
+
+const FADE_IN_VARIANTS = {
+  hidden: { opacity: 0, x: 0, y: 0 },
+  enter: { opacity: 1, x: 0, y: 0 },
+  exit: { opacity: 0, x: 0, y: 0 },
+};
+
+export const Wrapper = ({ children }: any) => {
+  return (
+    <motion.div
+      variants={FADE_IN_VARIANTS}
+      initial={FADE_IN_VARIANTS.hidden}
+      animate={FADE_IN_VARIANTS.enter}
+      exit={FADE_IN_VARIANTS.exit}
+      transition={{ duration: 0.75, type: 'linear' }}
+    >
+      <Container maxW='90%'>{children}</Container>
+    </motion.div>
+  );
+};
