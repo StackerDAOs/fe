3482f9b2cba6a550fb32546742f4b852d852afeb
Ryan Waits <ryan.waits@gmail.com>
2022-06-24 18:39:42 -0500

    add use token hook

---
+++ src/common/hooks/use-token.tsx
@@ -0,0 +1,80 @@
+// Hook (use-organization.tsx)
+import { useEffect, useState } from 'react';
+import { useNetwork, useCurrentStxAddress } from '@micro-stacks/react';
+import { fetchReadOnlyFunction } from 'micro-stacks/api';
+import { standardPrincipalCV } from 'micro-stacks/clarity';
+
+type TVotingExtension = {
+  isLoading: boolean;
+  contractAddress: string;
+  contractName: string;
+  balance: number;
+  symbol: string;
+};
+
+interface IVotingExtension {
+  tokenAddress: string;
+}
+
+const initialState = {
+  isLoading: true,
+  contractAddress: '',
+  contractName: '',
+  balance: 0,
+  symbol: '',
+};
+
+export function useToken(
+  { tokenAddress }: IVotingExtension = { tokenAddress: '' },
+) {
+  const [state, setState] = useState<TVotingExtension>(initialState);
+  const { network } = useNetwork();
+  const currentStxAddress = useCurrentStxAddress();
+
+  useEffect(() => {
+    async function fetchGovernanceToken() {
+      try {
+        const [contractAddress, contractName] = tokenAddress.split('.');
+        if (currentStxAddress && tokenAddress) {
+          const balance: any = await fetchReadOnlyFunction({
+            network,
+            contractAddress,
+            contractName,
+            senderAddress: currentStxAddress,
+            functionArgs: [standardPrincipalCV(currentStxAddress || '')],
+            functionName: 'get-balance',
+          });
+          const symbol: any = await fetchReadOnlyFunction({
+            network,
+            contractAddress,
+            contractName,
+            senderAddress: currentStxAddress,
+            functionArgs: [],
+            functionName: 'get-symbol',
+          });
+          setState({
+            ...state,
+            isLoading: false,
+            contractAddress,
+            contractName,
+            symbol,
+            balance,
+          });
+        }
+      } catch (e: any) {
+        console.log({ e });
+      } finally {
+        console.log('done');
+      }
+    }
+    fetchGovernanceToken();
+  }, [tokenAddress, currentStxAddress]);
+
+  return {
+    isLoading: state.isLoading,
+    contractAddress: state.contractAddress,
+    contractName: state.contractName,
+    balance: state.balance,
+    symbol: state.symbol,
+  };
+}
