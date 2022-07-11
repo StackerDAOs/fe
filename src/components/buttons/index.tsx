cdb437408b7da44501dfe7335d2c5e260cec1e83
Ryan Waits <ryan.waits@gmail.com>
2022-07-10 19:45:34 -0500

    add refactored action buttons

---
+++ src/components/buttons/index.tsx
@@ -0,0 +1,4 @@
+export * from './voting/VoteForButton';
+export * from './voting/VoteAgainstButton';
+export * from './voting/VoteManyButton';
+export * from './execution/ExecuteButton';
---
+++ src/components/buttons/voting/VoteManyButton.tsx
@@ -0,0 +1,122 @@
+import { useCallback, useState } from 'react';
+import type { ButtonProps } from '@chakra-ui/react';
+import { Button, Spinner, useToast } from '@chakra-ui/react';
+import { useOpenContractCall } from '@micro-stacks/react';
+import {
+  trueCV,
+  falseCV,
+  contractPrincipalCV,
+  listCV,
+  tupleCV,
+  noneCV,
+} from 'micro-stacks/clarity';
+import { TxToast } from '@components/Toast';
+import { generateWithDelegators, getDelegators } from '@common/functions';
+import { useDelegates, useExtension, useTransaction } from '@common/queries';
+
+// utils
+import { map, size } from 'lodash';
+import { contractPrincipal, getExplorerLink } from '@common/helpers';
+import { FaCheck } from 'react-icons/fa';
+
+type TVoteManyButtonProps = ButtonProps & {
+  text: string;
+  proposalPrincipal: string;
+  voteFor: boolean;
+};
+
+export const VoteManyButton = (props: TVoteManyButtonProps) => {
+  const toast = useToast();
+  const { text, proposalPrincipal, voteFor } = props;
+  const [transactionId, setTransactionId] = useState('');
+  const { extension: voting } = useExtension('Voting');
+  const { data: transaction } = useTransaction(transactionId);
+  const { data: delegatorData } = useDelegates();
+  const { openContractCall, isRequestPending } = useOpenContractCall();
+  const [proposalContractAddress, proposalContractName] =
+    contractPrincipal(proposalPrincipal);
+
+  const handleVote = useCallback(async () => {
+    const delegatorAddresses = map(delegatorData, 'delegatorAddress');
+    const delegateVote = listCV([
+      tupleCV({
+        for: voteFor ? trueCV() : falseCV(),
+        proposal: contractPrincipalCV(
+          proposalContractAddress,
+          proposalContractName,
+        ),
+        delegator: noneCV(),
+      }),
+    ]);
+    const delegators = getDelegators({
+      voteFor,
+      proposalContractAddress,
+      proposalContractName,
+      delegatorAddresses,
+    });
+    const hasDelegators = size(delegators) > 0;
+    const functionArgs = hasDelegators
+      ? [
+          listCV(
+            generateWithDelegators({
+              voteFor,
+              proposalContractAddress,
+              proposalContractName,
+              delegators,
+            }),
+          ),
+        ]
+      : [delegateVote];
+    const [contractAddress, contractName] = contractPrincipal(
+      voting?.contractAddress,
+    );
+    const functionName = 'vote-many';
+    const postConditions: any = [];
+
+    await openContractCall({
+      contractAddress,
+      contractName,
+      functionName,
+      functionArgs,
+      postConditions,
+      onFinish,
+      onCancel: () => {
+        console.log('Cancelled vote');
+      },
+    });
+  }, [proposalPrincipal]);
+
+  const decisionText = voteFor ? 'approve' : 'reject';
+  const onFinish = async (data: any) => {
+    setTransactionId(data.txId);
+    toast({
+      duration: 5000,
+      position: 'bottom-right',
+      isClosable: true,
+      render: () => (
+        <TxToast
+          message='Transaction submitted'
+          body={`Your vote to ${decisionText} has been submitted`}
+          url={getExplorerLink(data.txId)}
+          closeAll={toast.closeAll}
+        />
+      ),
+    });
+  };
+
+  const isPending = transaction?.tx_status === 'pending';
+  const isSuccessful = transaction?.tx_status === 'success';
+  const isDisabled = isRequestPending || isPending || isSuccessful;
+
+  return (
+    <Button {...props} onClick={handleVote} disabled={isDisabled}>
+      {isPending ? (
+        <Spinner size='xs' />
+      ) : isSuccessful ? (
+        <FaCheck fontSize='1rem' color='light.900' />
+      ) : (
+        text
+      )}
+    </Button>
+  );
+};
