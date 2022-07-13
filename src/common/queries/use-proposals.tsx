29fc7370524da43c768003e7d748aec71055a188
Ryan Waits <ryan.waits@gmail.com>
2022-07-13 16:21:55 -0500

    add new queries

--- src/common/queries/use-proposals.tsx
+++ src/common/queries/use-proposals.tsx
@@ -1,47 +1,22 @@
 // Hook (use-proposals.tsx)
-import { useQuery, useQueries } from 'react-query';
-import { useExtension } from '@common/queries';
-import { getProposal, getProposalCount, getProposalAddress } from '@common/api';
+import { useQuery } from 'react-query';
+import { useDAO } from '@common/queries';
+import { getDBProposals } from '@common/api';
 
-export function useProposals() {
-  const { extension: voting } = useExtension('Voting');
-  const { data } = useQuery(
-    ['proposals-count', 'count'],
-    () => getProposalCount(voting?.contractAddress),
+export function useProposals(filter = 'all') {
+  const { dao } = useDAO();
+
+  const { isFetching, isIdle, isLoading, isError, data } = useQuery(
+    ['proposals', dao?.name, filter],
+    async () => {
+      const data = await getDBProposals(dao?.id, filter);
+      return data;
+    },
     {
-      enabled: !!voting?.contractAddress,
+      enabled: !!dao,
+      refetchOnWindowFocus: false,
     },
   );
-  const proposalAddresses: any = [];
 
-  for (let proposalCount = Number(data); proposalCount > 0; proposalCount--) {
-    proposalAddresses.push({
-      queryKey: ['proposal-address', proposalCount?.toString()],
-      queryFn: async () => {
-        const data = await getProposalAddress(
-          voting?.contractAddress,
-          proposalCount?.toString(),
-        );
-        return data;
-      },
-      enabled: !!voting?.contractAddress,
-      refetchOnWindowFocus: false,
-    });
-  }
-  const results = useQueries(proposalAddresses);
-  const addresses = results.map((result: any) => {
-    return {
-      queryKey: ['proposal', result?.data],
-      queryFn: async () => {
-        const data = await getProposal(voting?.contractAddress, result?.data);
-        return data;
-      },
-      enabled: !!results && !!voting?.contractAddress && !!result?.data,
-      refetchOnWindowFocus: false,
-    };
-  });
-
-  const proposals = useQueries(addresses);
-
-  return { proposals, hasMore: proposals.length < Number(data) };
+  return { isFetching, isIdle, isLoading, isError, data };
 }
