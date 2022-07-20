01d9e1a955fa782a1a933d46c7ecb13e83c0d6f9
nate buch <nate.buch@gmail.com>
2022-07-20 00:42:30 -0500

    Add info popover on dao label

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,6 +6,11 @@ import {
   Stack,
   VStack,
   useBreakpointValue,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  SimpleGrid,
+  Text,
 } from '@chakra-ui/react';
 
 import { motion } from 'framer-motion';
@@ -14,8 +19,10 @@ import { FADE_IN_VARIANTS } from '@utils/animation';
 import { ustxToStx, convertToken } from '@common/helpers';
 import Avatar from 'boring-avatars';
 
+//components
 import { EmptyState } from '@components/EmptyState';
 import { Stat } from '@components/Stat';
+import { Card } from '@components/Card';
 
 import { defaultTo } from 'lodash';
 
@@ -161,18 +168,23 @@ export const Header = () => {
     <Container maxW='5xl'>
       <Stack spacing={{ base: '6', lg: '4' }} mt='5'>
         <Container>
-          <Stack
-            spacing='2'
-            mt='4'
-            mb='2'
-            direction={{ base: 'column', md: 'row' }}
-            justify='flex-start'
-            color='white'
+          <Popover
+            trigger='click'
+            openDelay={0}
+            placement='right'
+            defaultIsOpen={false}
           >
-            <VStack maxW='xl' spacing='3'>
-              <Link href={`/d/${dao}`}>
-                <a>
-                  <HStack align='baseline'>
+            <Stack
+              spacing='2'
+              mt='4'
+              mb='2'
+              direction={{ base: 'column', md: 'row' }}
+              justify='flex-start'
+              color='white'
+            >
+              <VStack maxW='xl' spacing='3'>
+                <PopoverTrigger>
+                  <HStack align='baseline' cursor='pointer'>
                     <Avatar
                       size={40}
                       name={DAO?.name}
@@ -194,10 +206,63 @@ export const Header = () => {
                       {DAO?.name}
                     </Heading>
                   </HStack>
-                </a>
-              </Link>
-            </VStack>
-          </Stack>
+                </PopoverTrigger>
+              </VStack>
+            </Stack>
+            <PopoverContent
+              borderColor='base.500'
+              _focus={{ outline: 'none' }}
+              bg='base.900'
+              w='auto'
+              my='2'
+            >
+              <SimpleGrid columns={{ base: 1 }}>
+                <Stack spacing='4' direction='row' p='3'>
+                  <Stack spacing='1'>
+                    <Card
+                      bg='transparent'
+                      border='none'
+                      minW='150px'
+                      px={{ base: '2', md: '2' }}
+                      py={{ base: '2', md: '2' }}
+                      _hover={{
+                        cursor: 'pointer',
+                        bg: 'base.900',
+                      }}
+                    >
+                      <VStack align='left'>
+                        <Text
+                          px='2'
+                          fontSize='m'
+                          fontWeight='regular'
+                          color='white'
+                        >
+                          DAO Info
+                        </Text>
+                        <Text
+                          px='2'
+                          fontSize='sm'
+                          fontWeight='regular'
+                          color='white'
+                        >
+                          Quorum: 12500
+                        </Text>
+                        <Text
+                          px='2'
+                          fontSize='sm'
+                          fontWeight='regular'
+                          color='white'
+                        >
+                          {'Discourse:' + ' '}
+                          <Link href='www.google.com'>www.google.com</Link>
+                        </Text>
+                      </VStack>
+                    </Card>
+                  </Stack>
+                </Stack>
+              </SimpleGrid>
+            </PopoverContent>
+          </Popover>
           <motion.div
             variants={FADE_IN_VARIANTS}
             initial={FADE_IN_VARIANTS.hidden}
